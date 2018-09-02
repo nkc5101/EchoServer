4fe42077c0ba0dd9325aa137ad1bd1b8e049b210
nate <nate@Desktop.firelocked.com>
2018-09-02 11:59:52 -0400

    added client ID

--- src/echoserver/Client.java
+++ src/echoserver/Client.java
@@ -1,4 +1,3 @@
-
 package echoserver;
 
 import java.io.BufferedReader;
@@ -7,57 +6,58 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-
 public class Client implements Runnable {
-    
-    private Socket clientSocket;
-    
-    public Client(Socket client) {
-        
+
+    private final Socket clientSocket;
+    private final int clientID;
+
+    public Client(Socket client, int clientID) {
+
         this.clientSocket = client;
-        
+        this.clientID = clientID;
+
     }
-    
+
     public void run() {
-        
+
         String line;
         BufferedReader in = null;
         PrintWriter out = null;
-        
+
         try {
-            
+
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
-            
+
             String inputLine;
-                
-                while ((inputLine = in.readLine()) != null) {
-                    
-                    System.out.println("Server: " + inputLine);
-                    inputLine = inputLine.toUpperCase();
-                    out.println(inputLine);
-                }
-            
+
+            while ((inputLine = in.readLine()) != null) {
+
+                System.out.println("Client " + clientID + ":" + inputLine);
+                inputLine = inputLine.toUpperCase();
+                out.println(inputLine);
+            }
+
         } catch (IOException e) {
-            
+
             e.printStackTrace();
-            
+
         }
-        
-        while(true) {
-            
+
+        while (true) {
+
             try {
-                
+
                 line = in.readLine();
                 out.println(line);
-                
+
             } catch (IOException e) {
-                
+
                 e.printStackTrace();
-                
+
             }
         }
-        
+
     }
-    
+
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,36 +1,36 @@
-
 package echoserver;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 
-
 public class EchoServer {
 
-
     public static void main(String[] args) {
-        
+
+        int clientID = 0;
+
         System.out.println("Simple Echo Server");
-        
+
         try (ServerSocket serverSocket = new ServerSocket(6000)) {
-            
+
             System.out.println("Waiting for connection....");
-            
-            while(true) {
-                
-            Client c;
-            c = new Client(serverSocket.accept());
-            Thread t = new Thread(c);
-            t.start();
-            
-            System.out.println("Connected to client");
-            
+
+            while (true) {
+
+                Client c;
+                clientID++;
+                c = new Client(serverSocket.accept(), clientID);
+                Thread t = new Thread(c);
+                t.start();
+
+                System.out.println("Connected to client");
+
             }
-            
+
         } catch (IOException ex) {
-            
+
             System.out.println(ex);
-            
+
         }
 
     }
