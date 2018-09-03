b19dabe116df9af2d4135d500f6a3752367e6f30
kken1 <kken1@10.0.0.25>
2018-09-02 20:30:35 -0400

    Feel free to make changes.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,6 +1,7 @@
 package echoserver;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
@@ -18,37 +19,63 @@ public class EchoClient {
             System.out.println("Waiting for a connection....");
             InetAddress localAddress = InetAddress.getLocalHost();
 
-            try (Socket clientSocket = new Socket(localAddress, 6000);
-                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                    BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            Socket clientSocket = new Socket(localAddress, 6000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                System.out.println("Connected to Server");
-                Scanner sc = new Scanner(System.in);
+            System.out.println("Connected to Server");
+            Scanner sc = new Scanner(System.in);
+            
+            Thread sendmsg = new Thread(new Runnable(){
+               
+                public void run() {
+                    
+                    System.out.println("Enter text:");
+                    
+                    while (true) {
+                        
+                        String inputLine = sc.nextLine();
 
-                while (true) {
+                        if ("quit".equalsIgnoreCase(inputLine)) {
 
-                    System.out.print("Enter text:");
-                    String inputLine = sc.nextLine();
+                            break;
 
-                    if ("quit".equalsIgnoreCase(inputLine)) {
+                        }
 
-                        break;
-
+                        out.println(inputLine);
+                        
                     }
-
-                    out.println(inputLine);
-
-                    String response = br.readLine();
-                    System.out.println("Server response: " + response);
-
+                    
                 }
-
-            } catch (Exception e) {
-
-                System.out.println(e);
-
-            }
-
+                
+            });
+            
+            Thread readmsg = new Thread(new Runnable() {
+                
+               public void run() {
+                   
+                   while (true) {
+                       
+                       try {
+                           
+                           String response = br.readLine();
+                           
+                           System.out.println("Server response: ");
+                           System.out.println(response);
+                           System.out.println("Enter text:");
+                           
+                       } catch (IOException e) {
+                           
+                           e.printStackTrace();
+                           
+                       }
+                   }
+               } 
+            });
+            
+            sendmsg.start();
+            readmsg.start();
+            
         } catch (Exception e) {
 
             System.out.println(e);
